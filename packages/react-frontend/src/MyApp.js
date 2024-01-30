a454f048f6b0f8fbdebeae4dce8a9d6633ac08af
AlexiLynn <agrabia@calpoly.edu>
2024-01-30 15:30:19 -0800

    We've reached the talking phase! (through step 2)

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,31 +1,44 @@
 // src/MyApp.js
-import React, { useState } from "react";
+import React, {useState, useEffect} from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
-
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
-    return (
-        <div className="container">
-          <Table
-            characterData={characters}
-            removeCharacter={removeOneCharacter}
-          />
-          <Form handleSubmit={updateList} />
-        </div>
-    );
-    
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => { console.log(error); });
+  }, [] );
+
+  const [characters, setCharacters] = useState([]);
+  return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={removeOneCharacter}
+        />
+        <Form handleSubmit={updateList} />
+      </div>
+  );
+
     function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
-        return i !== index;
+      return i !== index;
     });
     setCharacters(updated);
     }
 
     function updateList(person) {
-        setCharacters([...characters, person]);
-      }
+      setCharacters([...characters, person]);
+    }
+    
+    function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
+
 }
+
 
 export default MyApp;
